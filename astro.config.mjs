5f56e9983f7a7fae60db4725e8260b50549cb0b0
Adan Maidana <adanmaidana31@gmail.com>
2025-04-02 22:47:49 -0300

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://adanmaidana.github.io/Frontend-Mentor-Browser-extension-manager-UI/",
-  base: '/Frontend-Mentor-Browser-extension-manager-UI',
+  base: '/Frontend-Mentor-Browser-extension-manager-UI/',
   vite: {
     plugins: [tailwindcss()],
   },
